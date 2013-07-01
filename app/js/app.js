12fb04645b7831ba59605251b84f49ef5f27399c
Ben Charas <bcharas@andrew.cmu.edu>
2013-06-30 17:02:12 -0700

    web app work, adding heroku

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,21 @@
 'use strict';
-
-
+/*
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    //$routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    //$routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.when('/splash', {templateUrl: 'partials/splash.html', controller: 'MyCtrl2'});
     $routeProvider.when('/matches', {templateUrl: 'partials/matches.html', controller: 'PhoneListCtrl'});
+    $routeProvider.when('/splash', {templateUrl: 'partials/splash.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/splash'});
   }]);
+*/
+//'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'
+var myApp = angular.module('myApp',['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']);
+
+myApp.config(function($routeProvider, $httpProvider){
+    $routeProvider.when('/matches', {templateUrl: 'partials/matches.html', controller: 'MatchListCtrl'});
+    $routeProvider.when('/splash', {templateUrl: 'partials/splash.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/splash'});	
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    // //delete $httpProvider.defaults.headers.common['Origin'];
+});
+
